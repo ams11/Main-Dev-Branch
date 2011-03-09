bd2a923040308a790d12fe1d691cb3725a70226e
Joel McConaughy <joelmcconaughy@gmail.com>
2011-03-09 14:47:02 -0800

    Initial commit with working authlogic

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+V2::Application.config.secret_token = '7a473afa934f80a9757e244637fe35b28cd425a914637b820ada8162fa7411417b8d4cf9725dd1dbb3d8a8bbf0405488b49d807f3d0af07e09463583e21fe1f6'
