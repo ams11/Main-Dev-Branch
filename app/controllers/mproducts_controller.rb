25959c193774c728d2f4c636adcd0704b9789781
Joel McConaughy <joelmcconaughy@gmail.com>
2011-04-06 12:45:42 -0700

    CRUD working on mproduct

--- app/controllers/mproducts_controller.rb
+++ app/controllers/mproducts_controller.rb
@@ -42,18 +42,17 @@ class MproductsController < ApplicationController
 
   # GET /mproducts/1/edit
   def edit
-    @mproduct = Mproduct.new.find(params[:id], current_user.shopify_product_vendor)
+    @mproduct = Mproduct.new().find(params[:id], current_user.shopify_product_vendor)
   end
 
   # POST /mproducts
   # POST /mproducts.xml
   def create
-    @mproduct = Mproduct.new(params[:mproduct])
-    newp = @mproduct.save
-    Mproduct.add_product_to_custom_collection(@mproduct.id, "#{@mproduct.product_type}-#{@mproduct.product_category}") if newp
+    Rails.logger.debug "create:params = #{params.to_yaml}"
     
+    @mproduct = Mproduct.new(params[:mproduct])
     respond_to do |format|
-      if newp
+      if @mproduct.create
         format.html { redirect_to(mproducts_path, :notice => 'Mproduct was successfully created.') }
         format.xml  { render :xml => @mproduct, :status => :created, :location => @mproduct }
       else
@@ -66,10 +65,9 @@ class MproductsController < ApplicationController
   # PUT /mproducts/1
   # PUT /mproducts/1.xml
   def update
-    @mproduct = ShopifyAPI::Product.find(params[:id], :params => { :vendor => current_user.shopify_product_vendor })
-    
+    @mproduct = Mproduct.new.find(params[:id], current_user.shopify_product_vendor).update_attributes(params[:mproduct])
     respond_to do |format|
-      if @mproduct.update_attributes(params[:mproduct])
+      if @mproduct.save
         format.html { redirect_to(mproducts_path, :notice => 'Mproduct was successfully updated.') }
         format.xml  { head :ok }
       else
@@ -91,100 +89,3 @@ class MproductsController < ApplicationController
     end
   end
 end
-
-
-=begin
-class MproductsController < ApplicationController
-  filter_access_to :all
-
-  # GET /mproducts
-  # GET /mproducts.xml
-  def index
-    @mproducts = Mproduct.new(:vendor => current_user.shopify_product_vendor).find(:all)
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @mproducts }
-    end
-  end
-
-  # GET /mproducts/1
-  # GET /mproducts/1.xml
-  def show
-    @mproduct = Mproduct.new(:vendor => current_user.shopify_product_vendor).find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @mproduct }
-    end
-  end
-
-  # GET /mproducts/new
-  # GET /mproducts/new.xml
-  def new
-    @mproduct = Mproduct.new(:vendor => current_user.shopify_product_vendor)
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @mproduct }
-    end
-  end
-
-  # GET /mproducts/1/edit
-  def edit
-    @mproduct = Mproduct.new(:vendor => current_user.shopify_product_vendor).load(params[:id])
-
-    respond_to do |format|
-      format.html # edit.html.erb
-      format.xml  { render :xml => @mproduct }
-    end
-  end
-
-  # POST /mproducts
-  # POST /mproducts.xml
-  def create
-    @mproduct = Mproduct.new(params[:mproduct])
-
-    respond_to do |format|
-      if @mproduct.save
-        format.html { redirect_to(@mproduct, :notice => 'Mproduct was successfully created.') }
-        format.xml  { render :xml => @mproduct, :status => :created, :location => @mproduct }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @mproduct.errors, :status => :unprocessable_entity }
-      end
-    end
-  end
-
-  # PUT /mproducts/1
-  # PUT /mproducts/1.xml
-  def update
-    #@mproduct = Mproduct.find(params[:id])
-    @mproduct = nil
-
-    respond_to do |format|
-      if @mproduct.update_attributes(params[:mproduct])
-        format.html { redirect_to(@mproduct, :notice => 'Mproduct was successfully updated.') }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @mproduct.errors, :status => :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /mproducts/1
-  # DELETE /mproducts/1.xml
-  def destroy
-    #@mproduct = Mproduct.find(params[:id])
-    #@mproduct.destroy
-    @mproduct = nil
-
-    respond_to do |format|
-      format.html { redirect_to(mproducts_url) }
-      format.xml  { head :ok }
-    end
-  end
-end
-
-=end
--- app/models/mproduct.rb
+++ app/models/mproduct.rb
@@ -8,6 +8,7 @@ class Mproduct
   attr_accessor :id, :product_type, :product_category, :title, :body_html, :color, :gender, :brand, :onsale, :collectible, :custom, :vendor
   
   validates_presence_of :product_type, :product_category, :title, :body_html, :color, :gender, :brand, :onsale, :collectible, :custom, :vendor
+  #validates :price, :presence => true, :numericality => {:greater_than_or_equal_to => MERCHANT_CONFIG['constraints']['min_product_price'].to_f, :less_than_or_equal_to => MERCHANT_CONFIG['constraints']['max_product_price'].to_f}
   
   def self.smart_collections
     ShopifyAPI::SmartCollection.find(:all).size()
@@ -22,55 +23,112 @@ class Mproduct
   end
   
   def self.find_or_create_custom_collection(title)
-    if ShopifyAPI::CustomCollection.find(:all, :params => {:title => title}).size() > 0
-      ShopifyAPI::CustomCollection.find(:all, :params => {:title => title})
+    if c = ShopifyAPI::CustomCollection.find(:all, :params => {:title => title}).size > 0
+      ShopifyAPI::CustomCollection.find(:all, :params => {:title => title}).first
     else
       ShopifyAPI::CustomCollection.create(:title => title)
     end
-  end
-  
-  def self.add_product_to_custom_collection(product_id, collection_title)
-    c = find_or_create_custom_collection(collection_title)
-    ShopifyAPI::Collect.create(:product_id => product_id, :collection_id => c.id)
   end
   
   def initialize(attributes = {})
     attributes.each do |name, value|
       send("#{name}=", value)
     end
+    @attributes = attributes
   end
   
-  def find(id, vendor)
-    p = ShopifyAPI::Product.find(id, :params => { :vendor => vendor })
-    Rails.logger.debug "p = #{p.inspect}"
-    instance_variables.each do |name|
-      instance_variable_set(name, p[name])
-    end
+  def update_attributes(attributes = {})
+    initialize(attributes)
     self
   end
   
-  def save
-    if ret = valid?
-      attributes = {}
-      instance_variables.each do |name|
-        value = instance_variable_get(name)
-        attributes[name.sub(/^@/,'')] = value if !value.nil? and !value.empty?
-      end
-      attributes['tags'] = tags
-      p = ShopifyAPI::Product.create(attributes)
-      @id = p.id if p
-      p
+  def find(id, vendor)
+    @shopify_product = ShopifyAPI::Product.find(id, :params => { :vendor => vendor })
+    deserialize
+    self
+  end
+  
+  def create
+    return false if !valid?
+
+    add_tags_to_attributes!(@attributes)
+    p = ShopifyAPI::Product.create(@attributes)
+    
+    if p
+      @id = p.id
+
+      ret = p.add_metafield(ShopifyAPI::Metafield.new({
+        :namespace => "ext",
+        :key => "product_category",
+        :value => @product_category,
+        :value_type => 'string'
+      }))
+   
+      Mproduct.find_or_create_custom_collection("#{@product_type}-#{@product_category}").add_product(p)
     end
-    ret
+    p
+  end
+  
+  def save
+    return false if !valid?
+
+    add_tags_to_attributes!(@attributes)
+    @shopify_product.update_attributes(@attributes)
   end
   
   def persisted?
-    false
+    !@shopify_product.nil?
+  end
+  
+  private
+  
+  @shopify_product = nil
+  @attributes = {}
+  
+  def serialize
+    attributes = {}
+    instance_variables.each do |name|
+      value = instance_variable_get(name)
+      attributes[name.sub(/^@/,'')] = value if !value.nil? and !value.empty?
+    end
+    attributes['tags'] = tags
+    
+    return attributes
+  end
+
+  def deserialize
+    p = @shopify_product
+    
+     # simple scalar attributes
+    @id = p.id
+    @product_type = p.product_type
+    @title = p.title
+    @body_html = p.body_html
+    @vendor = p.vendor
+
+    # scalar metafields
+    p.metafields.each do |m|
+      case m.key
+        when "product_category" then instance_variable_set("@#{m.key}", m.value)
+      end
+    end
+    
+    # scalar tags
+    p.tags.split(",").each do |tag|
+      a = tag.strip.split(":",2)
+      case a.first
+        when "gender", "brand", "onsale", "collectible", "custom", "color" then instance_variable_set("@#{a.first}", a.last)
+      end
+    end
   end
   
   def tags
     "color:#{@color}, gender:#{@gender}, brand:#{@brand}, onsale:#{@onsale}, collectible:#{@collectible}, custom:#{@custom}"
   end
   
+  def add_tags_to_attributes!(a)
+    a['tags'] = "color:#{a['color']}, gender:#{a['gender']}, brand:#{a['brand']}, onsale:#{a['onsale']}, collectible:#{a['collectible']}, custom:#{a['custom']}"
   end
+  
+end
 
